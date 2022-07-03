bd96691033d7af30b3eedb7fc6fc1bc4b86db621
dwipusppa <dwipuspa009@gmail.com>
2022-07-03 12:43:39 +0700

    tambah baru

--- app/src/main/java/com/example/ngeruntah/view/ServiceInterface.kt
+++ app/src/main/java/com/example/ngeruntah/view/ServiceInterface.kt
@@ -6,14 +6,14 @@ import retrofit2.http.*
 
 class ServiceInterface {
     @GET("name")
-    fun getData(): Call<List<DataRiwayat>>
+    fun getData(): retrofit2.Call<List<DataRiwayat>>
     @GET("weight")
-    fun getData(): Call<List<DataRiwayat>>
+    fun getData(): retrofit2.Call<List<DataRiwayat>>
     @GET("date")
-    fun getData(): Call<List<DataRiwayat>>
+    fun getData(): retrofit2.Call<List<DataRiwayat>>
     @GET("address")
-    fun getData(): Call<List<DataRiwayat>>
+    fun getData(): retrofit2.Call<List<DataRiwayat>>
     @GET("note")
-    fun getData(): Call<List<DataRiwayat>>
+    fun getData(): retrofit2.Call<List<DataRiwayat>>
 
 }
