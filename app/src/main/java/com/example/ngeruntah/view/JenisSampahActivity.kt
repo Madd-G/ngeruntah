8b49caef51ba5039f0a2f57fea980e7fae34d9b4
dwipusppa <dwipuspa009@gmail.com>
2022-07-03 12:03:08 +0700

    tambah baru

--- app/src/main/java/com/example/ngeruntah/view/JenisSampahActivity.kt
+++ app/src/main/java/com/example/ngeruntah/view/JenisSampahActivity.kt
@@ -1,15 +1,8 @@
 package com.example.ngeruntah.view
 
-import android.app.Activity
-import android.content.Intent
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.View
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import com.example.ngeruntah.R
 import kotlinx.android.synthetic.main.activity_jenis_sampah.*
 
