e3d4261ef80b4feea9f336e228f102c8f784d39c
ChandraAyunda <candraayunda@gmail.com>
2022-07-03 11:26:13 +0700

    DataSampleRiwayat

--- app/src/main/java/com/example/ngeruntah/view/DataSampleRiwayat.kt
+++ app/src/main/java/com/example/ngeruntah/view/DataSampleRiwayat.kt
@@ -1,18 +1,29 @@
 package com.example.ngeruntah.view
 
 object DataSampleRiwayat {
-    private val email = arrayOf(
+    private val nama_pengguna = arrayOf(
     )
-    private val password = arrayOf(
+    private val jenis_sampah = arrayOf(
     )
-
-    val listData: ArrayList<DataLogin>
+    private val berat = arrayOf(
+    )
+    private val tanggal = arrayOf(
+    )
+    private val alamat = arrayOf(
+    )
+    private val catatan = arrayOf(
+    )
+    val listData: ArrayList<DataRiwayat>
         get() {
-            val listDataku = arrayListOf<DataLogin>()
-            for (position in email.indices) {
-                val data = DataLogin()
-                data.email = email[position]
-                data.password = password[position]
+            val listDataku = arrayListOf<DataRiwayat>()
+            for (position in nama_pengguna.indices) {
+                val data = DataRiwayat()
+                data.nama_pengguna = nama_pengguna[position]
+                data.jenis_sampah = jenis_sampah[position]
+                data.berat = berat[position]
+                data.tanggal = tanggal[position]
+                data.alamat = alamat[position]
+                data.catatan = catatan[position]
                 listDataku.add(data)
             }
             return listDataku
