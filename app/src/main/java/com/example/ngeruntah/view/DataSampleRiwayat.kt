c86840f993ee9707c4af7d97eeb06c513e04e229
NawangAnggitaWinanti <nawanga52@gmail.com>
2022-07-03 11:41:14 +0700

    arrayof

--- app/src/main/java/com/example/ngeruntah/view/DataSampleRiwayat.kt
+++ app/src/main/java/com/example/ngeruntah/view/DataSampleRiwayat.kt
@@ -2,16 +2,37 @@ package com.example.ngeruntah.view
 
 object DataSampleRiwayat {
     private val nama_pengguna = arrayOf(
+        "Chandra Ayunda Apta Soemedhy",
+        "Akhmad Nur Almasyah",
+        "Dwi Puspa",
+        "Nawang Anggita"
     )
     private val jenis_sampah = arrayOf(
+        "Organik",
+        "Anorganik",
+        "Organik",
+        "Anorganik"
     )
     private val berat = arrayOf(
+        "2",
+        "1.5",
+        "1",
+        "2.5"
     )
     private val tanggal = arrayOf(
+        "1-07-22",
+        "2-07-22",
+        "3-07-22",
+        "4-07-22"
     )
     private val alamat = arrayOf(
+        "Purwokerto selatan",
+        "purwokerto utara",
+        "purwokerto barat",
+        "purwokerto timur"
     )
     private val catatan = arrayOf(
+
     )
     val listData: ArrayList<DataRiwayat>
         get() {
